119db610c4a7144669d71d68172ac2ebfc51a80f
aacolin <72588048+aacolin@users.noreply.github.com>
2024-07-12 15:56:39 -0700

    Update ubuntu_btrfs--post-install.sh

--- ubuntu_btrfs--post-install.sh
+++ ubuntu_btrfs--post-install.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 sudo umount -a                  # Unmount all mounted partitions
-sudo mount /dev/sda1 /mnt       # Mount the root partition
+sudo mount /dev/sda3 /mnt       # Mount the root partition
 sudo cp -vF /etc/resolv.conf /mnt/etc/resolv.conf   # Copy the resolv.conf file to the new system
 sudo btrfs su snapshot /mnt /mnt/@          # Create a snapshot of the root subvolume
 sudo rm -rv /mnt/{bin,bin*,boot,cdrom,etc,home,lib,lib*,media,mnt,opt,root,run,sbin,sbin*,snap,srv,sys,tmp,usr,var}
 sudo btrfs su create /mnt/@{home,cache,log,tmp,snapshots}   # Create subvolumes for home, cache, log, tmp, and snapshots
 sudo cp -av /mnt/@/var/cache/* /mnt/@cache 
-rm -rv /mnt/@/var/cache/* || echo "No cache directory"    # Copy the cache directory to the cache subvolume
+rm -frv /mnt/@/var/cache/* || echo "No cache directory"    # Copy the cache directory to the cache subvolume
 sudo cp -av /mnt/@/var/log/* /mnt/@log
-rm -rv /mnt/@/var/log/* || echo "No log directory"    # Copy the log directory to the log subvolume
+rm -frv /mnt/@/var/log/* || echo "No log directory"    # Copy the log directory to the log subvolume
 sudo umount /mnt                                            # Unmount the root partition
+mkdir -p /mnt/.snapshots
 sudo mount -o subvol=@ /dev/sda3 /mnt                       # Mount the root subvolume
 sudo mount -o subvol=@home /dev/sda3 /mnt/home              # Mount the home subvolume
 sudo mount -o subvol=@cache /dev/sda3 /mnt/var/cache        # Mount the cache subvolume
